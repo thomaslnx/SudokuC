465e17ae32a50a73c32c6fccbc6c9b8a4e5999e0
thomaslnx <marcos.msilva10@gmail.com>
2018-06-13 11:47:53 -0300

    Headers for sudoku.c and puzzle.c

--- sudoku.h
+++ sudoku.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int ** criaPuzzle(); // Estudar sobre PONTEIROS e PONTEIRO para PONTEIRO nesse programa para entende o programa.
 void printPuzzle(int ** puzzle);
